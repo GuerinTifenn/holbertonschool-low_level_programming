60599d7b36523592e42482cd254c70b2cf394bef
root <8807@holbertonstudents.com>
2024-02-19 13:22:07 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,6 +2,7 @@
 # include <stdio.h>
 
 int main(void)
-	printf("Programming is like building a multilingual puzzle);
-
-	return 0;
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
