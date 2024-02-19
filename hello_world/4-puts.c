9b80717c181a7341445fe067293390d1d3ee257c
root <8807@holbertonstudents.com>
2024-02-19 13:49:14 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,11 @@
 # include <stdio.h>
 
 /**
- * main - the entry point of the program
+ * main - the entry point of the program and return 0
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
