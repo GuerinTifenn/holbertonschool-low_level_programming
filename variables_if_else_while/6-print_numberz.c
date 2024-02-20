4799fbca4f0455c0f6bb5bd0f5740421c2ff55f0
root <8807@holbertonstudents.com>
2024-02-20 12:22:56 +0000

    6-print_numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number = 0;
+
+	for (number = 0; number <= 9; number++)
+		putchar(number);
+	putchar('\n');
+	return (0);
+}
