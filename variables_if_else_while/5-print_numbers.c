ca4b9bd3c39c73e8d4b3e0ba31098a38efbcd3ec
root <8807@holbertonstudents.com>
2024-02-20 12:11:35 +0000

    5-print_numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number = 0;
+
+	for (number = 0; number <= 9; number++)
+		printf("%d", number);
+	return (0);
+}
