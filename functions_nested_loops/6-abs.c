17b1c225627006afd5fbe0a16374244e72d53aa4
GuerinTifenn <8807@holbertonstudents.com>
2024-02-21 19:46:45 +0000

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _abs - check the code
+ *@n: caracter
+ * Return: Always 0.
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+	return (-n);
+	}
+	else
+	{
+	return (n);
+	}
+}
