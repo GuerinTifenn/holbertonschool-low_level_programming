2fc6237b58f8fa53f37e6648af6af2f200bdccfa
GuerinTifenn <8807@holbertonstudents.com>
2024-02-21 18:43:16 +0000

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -8,16 +8,19 @@
  */
 int print_sign(int n)
 {
-	if (n > '1')
+	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
 	}
-	else if (n < '0')
+	else if (n < 0)
 	{
-		_putchar('-');
+		_putchar(45);
 		return (-1);
 	}
-	_putchar('0');
-	return (0);
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
 }
