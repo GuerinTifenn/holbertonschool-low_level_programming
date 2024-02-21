115f71ec58ea8d1e69c1d07dd62a966d877d643a
GuerinTifenn <8807@holbertonstudents.com>
2024-02-21 20:48:37 +0000

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_last_digit - check the code
+ *@n: caracter
+ * Return: Always 0.
+ */
+
+int print_last_digit(int n)
+{
+	int x = n % 10;
+
+	return (x);
+}
