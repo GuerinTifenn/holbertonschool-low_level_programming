750dc563d4ad1d62bcc9d4afc34ed640dbdd0798
GuerinTifenn <8807@holbertonstudents.com>
2024-02-21 17:45:34 +0000

    4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - check the code.
+ * @c: caracter
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
