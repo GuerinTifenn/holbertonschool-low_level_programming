1f7bdb8bbf1a61178b695aefc32375bf02aa9abb
GuerinTifenn <8807@holbertonstudents.com>
2024-02-24 14:43:15 +0000

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@ void print_to_98(int n)
 {
 	for (n = 0; n <= 98; n++)
 	{
-		_putchar (n);
+		printf("%d", n);
 		if (n < 98)
 		{
 			_putchar (',');
@@ -21,7 +21,7 @@ void print_to_98(int n)
 
 	for (n = 0; n >= 98; n--)
 	{
-		_putchar (n);
+		printf("%d", n);
 		if (n > 98)
 		{
 			_putchar (',');
