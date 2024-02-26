694b4e3c0ead3f026a30ff7ff4216963f3d8914c
GuerinTifenn <8807@holbertonstudents.com>
2024-02-26 09:47:09 +0000

    3-print_numbers.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -3,12 +3,13 @@
 
 /**
  * print_numbers - prints the numbers, from 0 to 9
- * @i: number
+ *
  * Return: Always 0.
  */
 
-void print_numbers(int i)
+void print_numbers(void)
 {
+	int i;
 
 	for (i = 0; i < 10; i++)
 	{
