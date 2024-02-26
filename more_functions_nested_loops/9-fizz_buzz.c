ae3c1caf85cebbe6166d322805c267ffe212cc02
GuerinTifenn <8807@holbertonstudents.com>
2024-02-26 15:10:16 +0000

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,13 +1,14 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * fizz_buzz - prints the numbers from 1 to 100 for numbers which are multiples of both three and five print FizzBuzz
+ * main - prints the numbers from 1 to 100 for 3 and 5 print FizzBuzz
  *
  * Return: Always 0.
  */
-int fizz_buzz(int i)
+int main(void)
 {
+	int i;
+
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 != 0 && i % 5 != 0 && i % 15 != 0)
