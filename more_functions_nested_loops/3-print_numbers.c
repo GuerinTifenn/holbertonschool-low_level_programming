35d6fe915b510f37cd03b113602c74fd1deeb605
GuerinTifenn <8807@holbertonstudents.com>
2024-02-26 09:42:37 +0000

    3-print_numbers.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -3,17 +3,16 @@
 
 /**
  * print_numbers - prints the numbers, from 0 to 9
- * @n: number
+ * @i: number
  * Return: Always 0.
  */
 
-void print_numbers(void)
+void print_numbers(int i)
 {
-	int n;
 
-	for (n = 0; n < 10; n++)
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(n + '0');
+		_putchar(i + '0');
 	}
 	 _putchar('\n');
 }
