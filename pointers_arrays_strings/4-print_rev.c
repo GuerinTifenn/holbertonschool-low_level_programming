411fa113a650cc3c65a67567a9c9a258278d9a61
GuerinTifenn <8807@holbertonstudents.com>
2024-02-27 12:03:55 +0000

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+  * _puts - prints a string
+  * @str: a pointer to a char
+  * Return: Always 0 (Success)
+  */
+
+void _puts(char *str)
+{
+	int i = 0;
+	int j = strlen(str) - 1;
+
+	while (i <= j)
+	{
+		putchar(str[j]);
+		j--;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
