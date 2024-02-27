e8292aba684c25284fa62f4c498ca013b3a48a62
GuerinTifenn <8807@holbertonstudents.com>
2024-02-27 14:29:21 +0000

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * puts2 - prints a string
+  * @str: a pointer to a char
+  * Return: Always 0 (Success)
+  */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
