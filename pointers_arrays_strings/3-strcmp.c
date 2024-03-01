236e61d6488d229ecd1714ba6aebac1909f2c95d
GuerinTifenn <8807@holbertonstudents.com>
2024-03-01 10:51:59 +0000

    3-strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,19 +10,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
-
-	i = 0;
-	j = 0;
-
-	while (s1[i] != '\0')
-		i++;
-
-	while (s2[j] != '\0')
-		j++;
-
-	if (*s1 == *s2)
-		return (0);
-	else
-		return (*s1 - *s2);
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0' && *s2 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
