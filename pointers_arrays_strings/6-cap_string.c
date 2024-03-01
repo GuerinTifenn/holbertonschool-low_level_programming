340fdc3fb93119400424511f5871fc71555b783f
GuerinTifenn <8807@holbertonstudents.com>
2024-03-01 14:02:55 +0000

    6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * *cap_string - capitalizes all words of a string
+ * @s: string to change
+ * subtract 32, which is the difference between the ASCII values 'A' and 'a'
+ * Return: string
+ */
+
+char *cap_string(char *s)
+{
+	char i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z' && s[i - 1] == ' ')
+		{
+			s[i] = s[i] - 32;
+		}   
+	}   
+	return (s);
+}
