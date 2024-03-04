394ac210c9c83240718828462f520af264803089
GuerinTifenn <8807@holbertonstudents.com>
2024-03-04 14:57:06 +0000

    3-strspn.c

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * _strspn - locates a character in a string
+ * @s: string to be scanned
+ * @accept: string containing the characters to match.
+ * Return: the number of characters
+ */
+ 
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0' && s[i] != accept[j])
+			j++;
+		if(accept[j] == '\0')
+			return (i);
+		i++;
+	}
+	return (i);
+}
