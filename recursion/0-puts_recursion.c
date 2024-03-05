c6da31dcc2576b81d6c95ad5e1ece0b6b6ab9b77
GuerinTifenn <8807@holbertonstudents.com>
2024-03-05 14:13:14 +0000

    0-puts_recursion.c

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * _puts_recursion - prints a string
+  * @s: a pointer to a char
+  */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		putchar(s[i]);
+		_puts_recursion(s + 1);
+	}
+	else
+		putchar('\n');
+}
