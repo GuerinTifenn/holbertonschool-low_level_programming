75a9c3cddbe656fd7b924af063b8b240231c3ef6
GuerinTifenn <8807@holbertonstudents.com>
2024-03-06 08:56:37 +0000

    2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,13 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int i;
+
+	while (s[i] >= '\0')
 	{
-		if (*s == c)
-		return (s);
-		s++;
-		if (*s == 0)
-			return (NULL);
+		if (s[i] == c)
+		return (s + i);
+		i++;
 	}
 	return ('\0');
 }
