ead86a2d0b62c362ed70ae3ec968607aaf500da8
GuerinTifenn <8807@holbertonstudents.com>
2024-03-15 09:15:59 +0000

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -19,6 +19,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		new_dog->name = name;
 		new_dog->age = age;
 		new_dog->owner = owner;
+		return (new_dog);
 	}
-	return (new_dog);
+	else
+		return (NULL);
 }
