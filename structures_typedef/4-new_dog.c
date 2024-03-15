1147a83d37fb5227f7877045e8dd24275a768ce2
GuerinTifenn <8807@holbertonstudents.com>
2024-03-15 09:08:54 +0000

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -4,19 +4,21 @@
 
 /**
  * new_dog - creates a new dog
- * @name: dog's name'.
- * @age: dog's age'.
+ * @name: dog's name'
+ * @age: dog's age'
  * @owner: dog owner's
+ * Return: The new struct dog
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog = (dog_t*)malloc(sizeof(dog_t));
-    if (new_dog)
-    {
-        new_dog->name = name;
-        new_dog->age = age;
+	dog_t *new_dog = (dog_t)malloc(sizeof(dog_t));
+
+	if (new_dog)
+	{
+		new_dog->name = name;
+		new_dog->age = age;
 		new_dog->owner = owner;
-    }
-    return new_dog;
+	}
+	return (new_dog);
 }
