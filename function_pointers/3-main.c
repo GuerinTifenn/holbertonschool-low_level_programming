ba1c4f98a48f09e9385397ed7656331de2649311
GuerinTifenn <8807@holbertonstudents.com>
2024-03-20 19:23:48 +0000

    all

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -12,30 +12,30 @@
 
 int main(int argc, char *argv[])
 {
-	int a, b;
-	int (*op_func)(int, int);
+	int num1, num2;
+	int (*operator)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		return (98);
-	}
-	if (argv[2][1] != '\0')
-	{
-		printf("Error\n");
-			return (99);
-	}
-	op_func = get_op_func(argv[2]);
-
-	if (op_func == NULL)
-	{
-		printf("Error\n");
-		return (100);
+		exit(98);
 	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	printf("%d\n", op_func(a, b));
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = get_op_func(argv[2]);
+
+	if (operator == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (num2 == 0 && (*argv[2] == '%' || *argv[2] == '/'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", operator(num1, num2));
 
 	return (0);
 }
