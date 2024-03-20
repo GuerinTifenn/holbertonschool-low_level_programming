dc0f6abf01a4cc3e0d2828135d5c4033b75ad148
GuerinTifenn <8807@holbertonstudents.com>
2024-03-20 17:27:00 +0000

    1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -3,7 +3,7 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers -  prints numbers
+ * print_numbers - prints numbers
  * @separator: the string to be printed between numbers
  * @n: the number of integers passed to the function
  */
