5775db001eaca17c72e8faecb6ba54d720befbd8
GuerinTifenn <8807@holbertonstudents.com>
2024-04-02 12:56:45 +0000

    8-delete_dnodeint.c

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -21,7 +21,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = (*head)->next;
-		if (*head != NULL)
+		if (*head)
 			(*head)->prev = NULL;
 		free(current);
 		return (1);
@@ -36,7 +36,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (-1);
 
 	current->prev->next = current->next;
-	if (current->next != NULL)
+	if (current->next)
 		current->next->prev = current->prev;
 	free(current);
 
