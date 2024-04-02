4b937671512cd195e687051ba8d409b1af22baae
GuerinTifenn <8807@holbertonstudents.com>
2024-04-02 14:09:10 +0000

    8-delete_dnodeint.c

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -4,7 +4,7 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - deletes the node at index of a dlistint_t linked list.
+ * delete_dnodeint_at_index - deletes node at index of a dlistint_t linked list
  * @head: pointer to the pointer to the head of the linked list
  * @index: index of the node to delete
  * Return: 1 if succeeded, -1 if failed
