36c88a6ebbfb4dd7302d89844ea834e32fbf2049
GuerinTifenn <8807@holbertonstudents.com>
2024-04-02 08:37:18 +0000

    erase strlen of 2-add_note

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,7 +1,6 @@
-#include "lists.h"
 #include <stdlib.h>
 #include <string.h>
-#include <stddef.h>
+#include "lists.h"
 
 /**
  * add_node - adds a new node at the beginning of a list_t list
@@ -14,21 +13,21 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->str = strdup(str);
-
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->len = strlen(str);
-	new_node->next = *head;
+	new_node->len = len;
+	new_node->next = (*head);
 	*head = new_node;
 
-	return (new_node);
+	return (*head);
 }
